2e1ee3edd1bd0e94e3e6fe4fc49bb2636bce029b
Israel Thalles <santosisrael14@gmail.com>
2023-05-16 09:56:14 -0300

    Início do algoritmo de Dijkstra

---
+++ app/services/Dijkstra.js
@@ -0,0 +1,20 @@
+export default function Dijkstra({grafo, origem, destino}) {
+    const newNode = {
+        id: 0,
+        x: 0,
+        y: 0,
+        vizinhos: [
+            {
+                x1: 0,
+                y1: 0,
+                x2: 2,
+                y2: 2,
+                cor: "gray",
+                peso: 1,
+                vizinho: 1
+            }
+        ]
+    }
+    
+    
+}
